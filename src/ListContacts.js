9e920e52ca9fbbed0e28c3ea25e349e3832ba0b2
Everett Grassler <dragwar9@gmail.com>
2019-01-18 23:47:02 -0600

    Functional Components
    State Management - Functional Components

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,28 +1,26 @@
 import React from 'react';
 
-class ListContacts extends React.Component {
-  render() {
-    return (
-      <ol id="ListContacts" className="contact-list">
-        {
-          this.props.contacts.map(contact => (
-            <li key={contact.id} className="contact-list-item" id={contact.id + "-contact"}>
-              <div
-                className="contact-avatar"
-                style={{
-                  backgroundImage: `url(${contact.avatarURL})`
-                }}></div>
-              <div className="contact-details">
-                <p className="contact-name">{contact.name}</p>
-                <p className="contact-handle">@{contact.handle}</p>
-              </div>
-              <button className="contact-remove">Remove</button>
-            </li>
-          ))
-        }
-      </ol>
-    );
-  }
+function ListContacts(props) {
+  return (
+    <ol id="ListContacts" className="contact-list">
+      {
+        props.contacts.map(contact => (
+          <li key={contact.id} className="contact-list-item" id={contact.id + "-contact"}>
+            <div
+              className="contact-avatar"
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`
+              }}></div>
+            <div className="contact-details">
+              <p className="contact-name">{contact.name}</p>
+              <p className="contact-handle">@{contact.handle}</p>
+            </div>
+            <button className="contact-remove">Remove</button>
+          </li>
+        ))
+      }
+    </ol>
+  );
 }
 
 export default ListContacts;
