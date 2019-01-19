4b269c0b881c66ffa597985f59e9bbdffdb0c49e
Everett Grassler <dragwar9@gmail.com>
2019-01-18 23:53:53 -0600

    State
    State Management - State (fixed the contacts data to reflect the avatars)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts';
 
-const contacts = [
-  {
-    "id": "karen",
-    "name": "Karen Isgrigg",
-    "handle": "karen_isgrigg",
-    "avatarURL": "./michael.jpg"
-  },
-  {
-    "id": "richard",
-    "name": "Richard Kalehoff",
-    "handle": "richardkalehoff",
-    "avatarURL": "./ryan.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "handle": "tylermcginnis",
-    "avatarURL": "./tyler.jpg"
-  }
-]
-
 class App extends Component {
+  state = {
+    contacts: [
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "handle": "michael_jackson",
+        "avatarURL": "./michael.jpg"
+      },
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "handle": "ryanflorence",
+        "avatarURL": "./ryan.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "handle": "tylermcginnis",
+        "avatarURL": "./tyler.jpg"
+      }
+    ],
+  }
   render() {
     return (
       <div className="App">
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     );
   }
