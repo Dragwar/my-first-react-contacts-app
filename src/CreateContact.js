2a242f7148b58baf41efa6ddb49125a5fa77c124
Everett Grassler <dragwar9@gmail.com>
2019-01-28 18:04:50 -0600

    Handle Submission
    React Router - Handle Submission

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize';
 
 class CreateContact extends Component {
+  handleSubmit = e => {
+    e.preventDefault();
+    const values = serializeForm(e.target, { hash: true })
+    console.log('form data', values);
+
+    if (this.props.onCreateContact) {
+      this.props.onCreateContact(values)
+    }
+  };
+
   render() {
     return (
       <div>
@@ -12,7 +23,7 @@ class CreateContact extends Component {
         >
           Close
         </Link>
-        <form className="create-contact-form">
+        <form onSubmit={(e) => this.handleSubmit(e)} className="create-contact-form">
           <ImageInput
             className="create-contact-avatar-input"
             name="avatarURL"
